b42bef1ca1bcf222db8f10a1b22a3f1d0c371473
Seongwook Jin <88529990+seongwoooook@users.noreply.github.com>
2026-02-16 03:58:31 +0900

    feat : day01 python basic syntax and f-string practice

--- notebooks/day01_basic.py
+++ notebooks/day01_basic.py
@@ -16,3 +16,15 @@ value = 10
 print(f"{value * 3}")
 
 print(f"내 나이는 {age:05d}살입니다")
+
+age = int(input("나이를 입력하세요: "))
+print(f"당신의 나이는 {age}살입니다.")
+
+if age >= 20:
+    print("성인입니다.")
+else:
+    print("미성년자입니다.")
+
+print(type(10))
+print(type("10"))
+print(type(10.5))
