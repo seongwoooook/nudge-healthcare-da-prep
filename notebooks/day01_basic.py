f97bdb64e9e082e531b4a72208b34f89aacb5ffd
Seongwook Jin <88529990+seongwoooook@users.noreply.github.com>
2026-02-16 03:48:32 +0900

    feat : day01 python basic syntax and f-string practice

---
+++ notebooks/day01_basic.py
@@ -0,0 +1,16 @@
+print ("Hello, Nudge!")
+
+a = 10
+b = 3
+
+print(a+b)
+print(a*b)
+
+age = 23
+print("내 나이는",age,"살입니다 !")
+print("내 나이는"+str(age)+"살입니다 !")
+print(f"내 나이는 {age}살입니다 !")
+print("내 나이는",age,"살입니다 !",sep="")
+
+value = 10
+print(f"{value * 3}")
